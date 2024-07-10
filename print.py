47aa83724a7f99f9a4fd43f44182c93b3740d116
LEEABOT <1483008211@qq.com>
2024-07-10 15:35:29 +0800

    更新

---
+++ print.py
@@ -0,0 +1,16 @@
+# print输出语句
+a="你好"
+b=14
+c=35
+
+print(13)
+print(a)
+print(b)
+print ("hello\tworld")# \t为Tab
+print ("1234\n4356")# \n为换行
+print(f"hi{c}")# 输出一个固定的hi后面跟一个变量c
+print("hi %d"%c)# 输出效果同上
+print("在输出末尾加东西", end="\t加的内容")
+"""
+今天是第一天
+"""
