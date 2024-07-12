2cdf2cf0912c5fecd21eb00311b8ccf5e87d3417
LEEABOT <1483008211@qq.com>
2024-07-12 14:39:04 +0800

    更新

--- function.py
+++ function.py
@@ -8,7 +8,12 @@ else :
     print(f"数字{num}是奇数")
 """
 
-def a (n,age=18,*args,**kwargs):
+def a (n,age=10,*args,**kwargs):
+    """
+    参数的顺序需要按照这个方式写，
+    先是普通参数，再默认参数，
+    再是包裹位置参数，最后是包裹关键字参数
+    """
     if age >=18:
         print(n)
         for i in args:
@@ -18,4 +23,4 @@ def a (n,age=18,*args,**kwargs):
     print(kwargs["ns"])
     print(kwargs["nt"])
 
-a(1,196,2,3,6,9,ns=15,nt=16)
+a(1,100,2,3,6,9,ns=15,nt=16)
